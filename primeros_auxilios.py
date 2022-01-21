f42605f0d575924cd8c45562bd6ad3d2b63c0b7b
FernandoCamousseigt <82519981+FernandoCamousseigt@users.noreply.github.com>
2022-01-21 12:01:16 -0300

    Add files via upload

---
+++ primeros_auxilios.py
@@ -0,0 +1,25 @@
+from traceback import print_tb
+
+
+respuesta = input('responde estimulos? y/n:  ')
+
+if respuesta == 'y':
+    print('esta sano')
+
+else:
+    print('abrir la via aerea')
+    respuesta = input('¿respira? y/n:  ')
+
+    if respuesta == 'y':
+        print('Permitirle posicion de suficiente ventilacion')
+
+    else:
+        print('Administrar 5 ventilaciones y llamar ambulancia')
+
+        while respuesta !='y':  #or == 'n':
+            respuesta = input('¿esta vivo? y/n:  ')
+            if respuesta == 'y':
+                print('Reevaluar a la espera de la ambulancia')
+            else:
+                print('Administrar compresiones')
+            respuesta = input('¿llego la ambulancia? y/n: ')
